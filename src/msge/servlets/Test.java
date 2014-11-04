c3447d53b39a46e1d546c53549e222a56276df5a
msge <tux55msge@gmail.com>
2014-11-04 22:20:34 +0100

    Utilisation du fichier bonjour.jsp à la place de PrintWriter

--- src/msge/servlets/Test.java
+++ src/msge/servlets/Test.java
@@ -1,8 +1,6 @@
 package msge.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,21 +28,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-		response.setCharacterEncoding("UTF-8");
-
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE html>");
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<meta charset=\"utf-8\" />");
-		out.println("<title>Test</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<p>Bonjour !</p>");
-		out.println("<p>URL: " + request.getRequestURL() + "</p>");
-		out.println("</body>");
-		out.println("</html>");
+		this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
 	}
 
 	/**
